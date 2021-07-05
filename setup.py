c9155a3394b10c7abddfddb66e9501e20fccb6d3
kaleissin <kaleissin@gmail.com>
2021-07-05 08:58:22 +0200

    Support current pythons
    - Remove all workarounds for Python 2
    - Drop support for Python 3.5
    - Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -37,9 +37,9 @@ setup(name='django-verification',
                 'Framework :: Django :: 3.1',
                 'Framework :: Django :: 3.2',
                 'Programming Language :: Python :: 3',
-                'Programming Language :: Python :: 3.5',
                 'Programming Language :: Python :: 3.6',
                 'Programming Language :: Python :: 3.7',
                 'Programming Language :: Python :: 3.8',
+                'Programming Language :: Python :: 3.9',
         ]
 )
